fc1f639845d4e26aa24663a660678f1477246f18
sandrambsp <sandrambspereira@gmail.com>
2021-12-22 13:17:58 +0000

    removed today from forecast

--- src/script.js
+++ src/script.js
@@ -39,7 +39,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7 && index > 0) {
       forecastHTML =
         forecastHTML +
         `
